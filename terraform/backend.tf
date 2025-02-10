13ed8ff9ee70b082361ccef516ec27d74a0d259a
Jose Acosta <acostasantana.jj@gmail.com>
2025-02-10 12:43:20 +0000

    Fix: Backend state S3 key

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "pocketcluster/terraform.tfstate"
+    bucket         = "acosta-pocket-cluster-state-bucket"
+    key            = "terraform.tfstate"
     region         = "eu-west-1"
     encrypt        = true
     dynamodb_table = "terraform-state-lock"
